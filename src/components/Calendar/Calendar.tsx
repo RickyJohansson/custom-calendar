cdd92b93babbd87a28097e69bed93fb7eca0efed
RickyJohansson <Rickjoh91@hotmail.com>
2023-10-10 13:08:54 +0200

    updated dates - custom arrows added

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -4,10 +4,11 @@ import { useEffect, useState } from 'react';
 
 const Calendar = () => {
 
-    const date = new Date();
-    const currentYear = date.getFullYear();
-    const currentMonth = date.getMonth();
-    const daysThisMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const date: Date = new Date();
+    const currentYear: number = date.getFullYear();
+    const currentMonth: number = date.getMonth();
+    const daysThisMonth: number = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const monthString: string = date.toLocaleString('default', { month: 'long' }).toUpperCase();
 
     const [ dayArray, setDayArray ] = useState<number[]>([]);
 
@@ -27,17 +28,39 @@ const Calendar = () => {
 
         <div className="calendar_border">
 
-            <div className="calendar_container">
+            <section className="calendar_container">
 
-                {
+                <section className="calendar_navigator">
 
-                    dayArray.map((day) => {
-                        return < CalendarDate day={day}/>
-                    })
+                    <figure className="left_arrow">
+                        <section className="arrow_shaft"></section>
+                        <section className="arrow_pointer"></section>
+                        <section className="arrow_block"></section>
+                    </figure>
 
-                }
+                    <h2>{ monthString }</h2>
 
-            </div>
+                    <figure className="right_arrow">
+                        <section className="arrow_shaft"></section>
+                        <section className="arrow_pointer"></section>
+                        <section className="arrow_block"></section>
+                    </figure>
+
+                </section>
+
+                <section className="dates">
+
+                    {
+
+                        dayArray.map((day) => {
+                            return < CalendarDate day={day}/>
+                        })
+
+                    }
+
+                </section>
+
+            </section>
             
         </div>
 
