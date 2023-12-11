4815c8b12fc0bb8b8c589ebc981fedd90d250fb4
RickyJohansson <Rickjoh91@hotmail.com>
2023-12-11 23:26:56 +0100

    reorder

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ let db;
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
+connectToDb((err) => {
+    if (!err) {
+        app.listen( PORT , () => {
+        console.log(`server running on port ${PORT}`)
+        })
+        db = getDb();
+    }
+});
+
 app.get("/activities" ,(req, resp) => {
     const activities = [];
     db.collection('activities').find().sort({ time: 1 }).forEach(activity => activities.push(activity))
@@ -20,12 +29,4 @@ app.get("/activities" ,(req, resp) => {
     resp.json({ "msg": "hello man" });
 })
 
-connectToDb((err) => {
-    if (!err) {
-        app.listen( PORT , () => {
-        console.log(`server running on port ${PORT}`)
-        })
-        db = getDb();
-    }
-  });
 
